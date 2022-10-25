f8fa874d9439bd20ff2a1291e5e2793b9d5420fc
alexanderrodin <pass.9157@gmail.com>
2022-10-25 20:36:11 +0300

    adding dependencies to the project

---
+++ app/src/main/java/com/example/firebaselitechat/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.firebaselitechat
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message").child("Sergey")
+
+        myRef.setValue("Hello, World!")
+
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "Value is: " + value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
+    }
+
+
+}
